9ff8bf32118de6e9f649dc88666dbf022ceeb4d7
farhanshahriyarabir62 <71967231+farhanshahriyarabir62@users.noreply.github.com>
2021-01-22 18:42:05 +0600

    answered kilometertometer question

---
+++ assignment.js
@@ -0,0 +1,11 @@
+//https://github.com/farhanshahriyar/Javascript-
+
+// kilometerToMeter
+
+function kilometerToMeter(kilometer) {
+    var getMeter = kilometer * 1000;
+    return getMeter;
+}
+
+var resultMeter = kilometerToMeter(1);
+console.log(resultMeter);
