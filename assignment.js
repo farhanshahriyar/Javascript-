b55151fd1739ecce8a271bd300d75da5c61cd770
farhanshahriyarabir62 <71967231+farhanshahriyarabir62@users.noreply.github.com>
2021-01-22 18:50:37 +0600

    answered 2nd question

--- assignment.js
+++ assignment.js
@@ -9,3 +9,18 @@ function kilometerToMeter(kilometer) {
 
 var resultMeter = kilometerToMeter(1);
 console.log(resultMeter);
+
+
+
+// budgetCalculator 
+
+function budgetCalculator(watch, phone, laptop) {
+    var watchPrice = watch * 250;
+    var phonePrice = phone * 24500;
+    var laptopPrice = laptop * 600000;
+    var getTotalPrice = watchPrice + phonePrice + laptopPrice;
+    return getTotalPrice;
+}
+
+var budgetResult = budgetCalculator(1,2,3);
+console.log(budgetResult);
